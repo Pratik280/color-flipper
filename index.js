5de11da11ec0ca423bbca31ff12d5c7f2fc7126a
Pratik280 <pratikchandlekar280@gmail.com>
2021-12-05 15:58:10 +0530

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello")
+let btn = document.querySelector(".btn");
+let textValue = document.querySelector(".main__color");
+let body = document.querySelector("body");
+
+var currentColor;
+
+function changeColor(e) {
+  var currentColor = Math.floor(Math.random()*16777215).toString(16);
+  textValue.textContent = "#" + currentColor;
+  body.style.background = "#" + currentColor;
+}
+
+btn.onclick = changeColor;
+
+// text value and bg color = current color
